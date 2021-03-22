b650aada3e1da07bfe0cb47efd9e76005bb53b96
User <semenetsbeph@gmail.com>
2021-03-22 22:59:05 +0100

    heroku deploy

--- src/main/java/com/example/loginapp/LoginAppApplication.java
+++ src/main/java/com/example/loginapp/LoginAppApplication.java
@@ -2,12 +2,20 @@ package com.example.loginapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginAppApplication {
+public class LoginAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginAppApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(LoginAppApplication.class);
 	}
 
 }
